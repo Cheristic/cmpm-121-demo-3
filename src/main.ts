1dcfd9b3d111343e97c95bb9e237188e3a768403
Cheristic <ethanheffan@gmail.com>
2024-11-01 18:28:46 -0700

    L16 init

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
-// todo
+const app = document.getElementById("app") as HTMLElement;
+
+const button = document.createElement("button");
+button.innerHTML = "COOL TEST";
+button.addEventListener("click", () => {
+  alert(`wow you're pretty cool`);
+});
+app.append(button);
