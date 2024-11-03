06b4eb7f49ac813d602d3c5bafb8fbe1d478ed9b
Cheristic <ethanheffan@gmail.com>
2024-11-02 22:48:13 -0700

    D3.b convert to null island coordinates

--- src/main.ts
+++ src/main.ts
@@ -11,8 +11,7 @@ import "./leafletWorkaround.ts";
 // Deterministic random number generator
 import luck from "./luck.ts";
 
-// Location of our classroom (as identified on Google Maps)
-const OAKES_CLASSROOM = leaflet.latLng(36.98949379578401, -122.06277128548504);
+import { Board } from "./board.ts";
 
 // Tunable gameplay parameters
 const GAMEPLAY_ZOOM_LEVEL = 19;
@@ -20,9 +19,14 @@ const TILE_DEGREES = 1e-4;
 const NEIGHBORHOOD_SIZE = 8;
 const CACHE_SPAWN_PROBABILITY = 0.1;
 
+const board = new Board(TILE_DEGREES, NEIGHBORHOOD_SIZE);
+
+// Location of our classroom (as identified on Google Maps)
+const START_LOCATION = leaflet.latLng(36.98949379578401, -122.06277128548504);
+
 // Create the map (element with id "map" is defined in index.html)
 const map = leaflet.map(document.getElementById("map")!, {
-  center: OAKES_CLASSROOM,
+  center: START_LOCATION,
   zoom: GAMEPLAY_ZOOM_LEVEL,
   minZoom: GAMEPLAY_ZOOM_LEVEL,
   maxZoom: GAMEPLAY_ZOOM_LEVEL,
@@ -40,7 +44,7 @@ leaflet
   .addTo(map);
 
 // Add a marker to represent the player
-const playerMarker = leaflet.marker(OAKES_CLASSROOM);
+const playerMarker = leaflet.marker(START_LOCATION);
 playerMarker.bindPopup("You are here. Hello here.");
 playerMarker.addTo(map);
 
@@ -52,11 +56,8 @@ statusPanel.innerHTML = "No points yet...";
 // Add caches to the map by cell numbers
 function spawnCache(i: number, j: number) {
   // Convert cell numbers into lat/lng bounds
-  const origin = OAKES_CLASSROOM;
-  const bounds = leaflet.latLngBounds([
-    [origin.lat + i * TILE_DEGREES, origin.lng + j * TILE_DEGREES],
-    [origin.lat + (i + 1) * TILE_DEGREES, origin.lng + (j + 1) * TILE_DEGREES],
-  ]);
+  const point = board.addCell(i, j)!;
+  const bounds = board.getCellBounds(point);
 
   // Add a rectangle to the map to represent the cache
   const rect = leaflet.rectangle(bounds);
@@ -108,8 +109,16 @@ function spawnCache(i: number, j: number) {
 }
 
 // Look around the player's neighborhood for caches to spawn
-for (let i = -NEIGHBORHOOD_SIZE; i < NEIGHBORHOOD_SIZE; i++) {
-  for (let j = -NEIGHBORHOOD_SIZE; j < NEIGHBORHOOD_SIZE; j++) {
+for (
+  let i = -NEIGHBORHOOD_SIZE + board.getCellForPoint(START_LOCATION).i;
+  i < NEIGHBORHOOD_SIZE + board.getCellForPoint(START_LOCATION).i;
+  i++
+) {
+  for (
+    let j = -NEIGHBORHOOD_SIZE + board.getCellForPoint(START_LOCATION).j;
+    j < NEIGHBORHOOD_SIZE + board.getCellForPoint(START_LOCATION).j;
+    j++
+  ) {
     // If location i,j is lucky enough, spawn a cache!
     if (luck([i, j].toString()) < CACHE_SPAWN_PROBABILITY) {
       spawnCache(i, j);
